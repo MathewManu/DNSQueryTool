1d83b049f3fa6cc1ffd66b88934f7f58ef033f56
manu mathew <manumathew.mec@gmail.com>
2018-02-13 07:26:56 -0800

    new flow through builder. Base case works.

---
+++ src/dnsquery/DNSResolver.java
@@ -0,0 +1,126 @@
+package dnsquery;
+
+import java.io.IOException;
+import java.net.UnknownHostException;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.xbill.DNS.DClass;
+import org.xbill.DNS.Message;
+import org.xbill.DNS.Name;
+import org.xbill.DNS.Record;
+import org.xbill.DNS.Section;
+import org.xbill.DNS.SimpleResolver;
+import org.xbill.DNS.TextParseException;
+import org.xbill.DNS.Type;
+
+public class DNSResolver {
+
+	private Name name = null;
+	
+	public void resolve(Dig digQuery) {
+		setName(digQuery.getURL());
+		try {
+			Message response = sendQuery(digQuery);
+			responseHandler(response);
+		} catch (IOException e) {
+
+			e.printStackTrace();
+		}
+
+	}
+
+
+	/*
+	 * create a newQuery using the user entered URL send the query to the server
+	 * based on the resolver. return : response Message from the server.
+	 */
+	private Message sendQuery(Dig digQuery) throws IOException {
+		Message msg = Message.newQuery(Record.newRecord(getName(), Type.A, DClass.IN));
+		return digQuery.getResolver().send(msg);
+
+	}
+
+/*	private Message sendQuery(Dig digQuery) throws IOException {
+		Message msg = Message.newQuery(Record.newRecord(getName(), Type.A, DClass.IN));
+		return digQuery.getResolver().send(msg);
+
+	}*/
+	private void responseHandler(Message res) {
+
+		List<Record> myList = new ArrayList<>();
+		int answerSectionCount = res.getHeader().getCount(Section.ANSWER);
+		System.out.println("count is :" + answerSectionCount);
+
+		if (answerSectionCount == 0) {
+			Record[] rec = res.getSectionArray(Section.AUTHORITY);
+			for (int i = 0; i < rec.length; i++) {
+				System.out.println(rec[i].getAdditionalName());
+				myList.add(rec[i]);
+			}
+			// as of now invoke with first result.
+			// getAdditionalName gives the address from the result.
+			invokeNextLevelQuery(myList.get(0).getAdditionalName().toString());
+
+		} else {
+			System.out.println("answer section found !!!!");
+			/*
+			 * There could be cases like the following. 1. Answer section has Both CNAME & A
+			 * record. In that case, returning the A record IP should be good enough 2.
+			 * Answer section only has CNAME. In this case, we would need to again query for
+			 * the CNAME recursively. eg: www.cs.stonybrook.edu.
+			 */
+
+			Record[] rec = res.getSectionArray(Section.ANSWER);
+			for (int i = 0; i < rec.length; i++) {
+				System.out.println(rec[i]);
+				// System.out.println(rec[i].getAdditionalName());
+				myList.add(rec[i]);
+			}
+		}
+
+	}
+	
+	private void invokeNextLevelQuery(String nextLevelUrl) {
+		System.out.println("====Next Leve========");
+		
+		Message res = null;
+		Message req = Message.newQuery(Record.newRecord(getName(), Type.A, DClass.IN));
+		SimpleResolver resolver = getResolver(nextLevelUrl);
+
+		try {
+			if (null != resolver) {
+				res = resolver.send(req);
+			}
+		} catch (IOException e) {
+			System.out.println("ERROR: while sending : " + getName() + " " + nextLevelUrl);
+		}
+		responseHandler(res);
+
+	}
+	
+	private static SimpleResolver getResolver(String name) {
+		SimpleResolver r = null;
+		try {
+			r = new SimpleResolver(name);
+		} catch (UnknownHostException e) {
+			System.out.println("ERROR: Unknown Host : " + name);
+		}
+		return r;
+	}
+
+	/* returns Name object */
+	private Name getName() {
+		return name;
+	}
+
+	private void setName(String url) {
+		try {
+			name = new Name(url);
+		} catch (TextParseException e) {
+			System.out.println("ERROR: " + url);
+		}
+
+	}
+
+}
--- src/dnsquery/Dnsquery.java
+++ src/dnsquery/Dnsquery.java
@@ -1,23 +1,30 @@
 package dnsquery;
 
 import java.io.IOException;
-import java.net.InetAddress;
-import java.net.UnknownHostException;
-
 import org.xbill.DNS.*;
 
 public class Dnsquery {
-
-	// static SimpleResolver r;
 
 	public static void main(String[] args) throws TextParseException {
 		// append . at the end of hostname here.
 
 		// DnsQueryProcessor.setquery("www.cnn.com.");
 		 //DnsQueryProcessor.setquery("www.cs.stonybrook.edu.");
-		DnsQueryProcessor.setquery("ec2-107-22-178-157.compute-1.amazonaws.com.");
-		DnsQueryProcessor.process();
+		//DnsQueryProcessor.setquery("ec2-107-22-178-157.compute-1.amazonaws.com.");
+		//DnsQueryProcessor.process();
 
+		DNSResolver resolver = new DNSResolver();
+		resolver.resolve(getDigQuery(args));
+	}
+
+	private static Dig getDigQuery(String[] args) {
+		
+		String name = "www.cs.stonybrook.edu.";
+		String type = null;
+		
+		return new Dig.QueryBuilder(name)
+				.withType(type).build();
+		
 	}
 }
 	
